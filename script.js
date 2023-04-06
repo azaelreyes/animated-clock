bf040d00feed476cead1956988b30a237365aec6
azaelreyes <azaelreyes20.ar@gmail.com>
2023-04-06 08:29:19 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,107 @@
+function clock(){
+  const now = new Date();
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext('2d');
+
+  //Setup Canvas
+  ctx.save();//save default state
+
+  ctx.clearRect(0,0,500,500); 
+  ctx.translate(250,250); // Put 0,0 in the center
+  ctx.rotate(-Math.PI/2); //Rotate clock -90 deg
+
+  //Set Default Styles
+  ctx.strokeStyle = '#000000'
+  ctx.fillStyle = '#f4f4f4';
+  ctx.lineWidth = 5;
+  ctx.lineCap = 'round';
+
+  //Draw Clock face/Border
+  ctx.save();
+  ctx.beginPath();
+  ctx.lineWidth = 14;
+  ctx.strokeStyle = '#800000';
+  ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
+  ctx.stroke();
+  ctx.fill();
+  ctx.restore()
+
+  //Draw Hour Lines
+  ctx.save();
+  for (let i = 0; i<12;i++){
+    ctx.beginPath();
+    ctx.moveTo(100,0)
+    ctx.lineTo(125,0);
+    ctx.stroke();
+    ctx.rotateText
+    ctx.rotate(Math.PI/6)
+  }
+  ctx.restore();
+
+  //Draw Minute Lines
+  ctx.save();
+  ctx.lineWidth = 4;
+  for (let i = 0; i<60;i++){
+    ctx.beginPath();
+    ctx.moveTo(117,0)
+    ctx.lineTo(120,0);
+    ctx.stroke();
+    ctx.rotateText
+    ctx.rotate(Math.PI/30)
+  }
+  ctx.restore();
+
+  //Get Current TIme
+  const hr = now.getHours() %12;
+  const min  = now.getMinutes();
+  const sec = now.getSeconds();
+  console.log(`${hr}:${min}:${sec}`)
+
+ // Write Hours
+  ctx.save();
+  ctx.strokeStyle = '#800000';
+  ctx.rotate(
+    (Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec
+  );
+  ctx.lineWidth = 14;
+  ctx.beginPath();
+  ctx.moveTo(-20, 0);
+  ctx.lineTo(80, 0);
+  ctx.stroke();
+  ctx.restore();
+
+  // Draw minute hand
+  ctx.save();
+  ctx.strokeStyle = '#800000';
+
+  ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(-28, 0);
+  ctx.lineTo(112, 0);
+  ctx.stroke();
+  ctx.restore();
+
+  // Draw second hand
+  ctx.save();
+  ctx.rotate((sec * Math.PI) / 30);
+  ctx.strokeStyle = '#FF7F50';
+  ctx.fillStyle = '#FF7F50';
+  ctx.lineWidth = 6;
+  ctx.beginPath();
+  ctx.moveTo(-30, 0);
+  ctx.lineTo(100, 0);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
+  ctx.fill();
+  ctx.restore();
+
+
+
+  ctx.restore(); //restore default state
+  requestAnimationFrame(clock);
+
+}
+
+requestAnimationFrame(clock);
